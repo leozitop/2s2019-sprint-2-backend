ad9069d9f89a44ce7e7b3d71561b0c6531ec0b5f
Leonardo Do Nascimento Pereira <45684686800@info.local>
2019-08-26 11:25:01 -0300

    Projetos

---
+++ Gufos/Senai.Gufos.WebApi/Senai.Gufos.WebApi/Controllers/CategoriasController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Gufos.WebApi.Domains;
+using Senai.Gufos.WebApi.Repositories;
+
+namespace Senai.Gufos.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        CategoriasRepository CategoriasRepository = new CategoriasRepository();
+
+        [HttpGet]
+        public IActionResult ListarTodos()
+        {
+            return Ok(CategoriasRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Categorias categoria)
+        {
+            try
+            {
+                CategoriasRepository.Cadastrar(categoria);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { mensagem = "Ih, deu erro." + ex.Message });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            Categorias Categoria = CategoriasRepository.BuscarPorId(id);
+            if (Categoria == null)
+                return NotFound();
+            return Ok(Categoria);
+        }
+
+        /// <summary>
+        /// Deletar uma categoria por id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Sucesso caso seja deletado.</returns>
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            CategoriasRepository.Deletar(id);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Atualizar(Categorias categoria)
+        {
+            try
+            {
+                Categorias CategoriasBuscada = CategoriasRepository.BuscarPorId
+                    (categoria.IdCategoria);
+
+                if (CategoriasBuscada == null)
+                    return NotFound();
+
+                CategoriasRepository.Atualizar(categoria);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ Gufos/Senai.Gufos.WebApi/Senai.Gufos.WebApi/Repositories/CategoriasRepository.cs
@@ -0,0 +1,80 @@
+﻿using Senai.Gufos.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Gufos.WebApi.Repositories
+{
+    public class CategoriasRepository
+    {
+        // nao ira mudar o acesso ao banco de dados, nao irei deixar de acessa-lo
+        // o que ira mudar eh como eu faco esse acesso
+        /// <summary>
+        /// Listar todas as categorias
+        /// </summary>
+        /// <returns>Lista de Categorias</returns>
+        public List<Categorias> Listar()
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // SELECT * FROM Categorias
+                return ctx.Categorias.ToList();
+            }
+        }
+
+        // Cadastrar
+        public void Cadastrar(Categorias categoria)
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // INSERT INTO
+                ctx.Categorias.Add(categoria);
+                ctx.SaveChanges();// efetiva as mudanças no banco de dados
+            }
+        }
+
+        // BuscarPorId
+        public Categorias BuscarPorId (int id)
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // select com where
+                // id da nossa tabela seja igual ao is enviado pelo usuario
+                return ctx.Categorias.FirstOrDefault(x => x.IdCategoria == id);
+            }
+        }
+
+        // Atualizar 
+        public void Atualizar(Categorias categoria)
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // busco a categoria
+                Categorias CategoriaBuscada = ctx.Categorias.FirstOrDefault(x => 
+                x.IdCategoria == categoria.IdCategoria);
+                // SET
+                CategoriaBuscada.Nome = categoria.Nome;
+                // atualizo no contexto
+                ctx.Categorias.Update(CategoriaBuscada);
+                // efetivo no database
+                ctx.SaveChanges();
+            }
+        }
+        
+        // Deletar
+        public void Deletar(int id)
+        {
+            using (GufosContext ctx = new GufosContext())
+            {
+                // DELETE FROM Categorias WHERE IdCategoria = @Id;
+                // encontrar quem eu quero deletar
+                Categorias CategoriaBuscada = ctx.Categorias.Find(id);
+                // remover o fofinho do contexto
+                ctx.Categorias.Remove(CategoriaBuscada);
+                // efetivar no banco essa mudança
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
